f7e4634f308bc75dfc4fb7d26858a940bb78e0ad
Daniel Ecer <de-code@users.noreply.github.com>
2017-11-21 15:56:15 +0000

    added simple pipeline test

--- sciencebeam_lab/preprocess/preprocessing_pipeline_test.py
+++ sciencebeam_lab/preprocess/preprocessing_pipeline_test.py
@@ -1,8 +1,59 @@
+from contextlib import contextmanager
+from mock import Mock, patch, DEFAULT
+
 import pytest
 
+from apache_beam.testing.test_pipeline import TestPipeline
+
 from sciencebeam_lab.preprocess.preprocessing_pipeline import (
-  parse_args
+  parse_args,
+  configure_pipeline
 )
+
+TestPipeline.__test__ = False
+
+PREPROCESSING_PIPELINE = 'sciencebeam_lab.preprocess.preprocessing_pipeline'
+
+def patch_preprocessing_pipeline():
+  return patch.multiple(
+    PREPROCESSING_PIPELINE,
+    find_file_pairs_grouped_by_parent_directory=DEFAULT
+  )
+
+def get_default_args():
+  return parse_args(['--data-path=test', '--pdf-path=test', '--xml-path=test'])
+
+@pytest.mark.filterwarnings('ignore::DeprecationWarning')
+@pytest.mark.filterwarnings('ignore::UserWarning')
+class TestConfigurePipeline(object):
+  def test_should_pass_pdf_and_xml_patterns_to_find_file_pairs_grouped_by_parent_directory(self):
+    with patch_preprocessing_pipeline() as mocks:
+      opt = get_default_args()
+      opt.base_data_path = 'base'
+      opt.pdf_path = 'pdf'
+      opt.xml_path = 'xml'
+      with TestPipeline() as p:
+        mocks['find_file_pairs_grouped_by_parent_directory'].return_value = []
+        configure_pipeline(p, opt)
+
+      mocks['find_file_pairs_grouped_by_parent_directory'].assert_called_with(
+        ['base/pdf', 'base/xml']
+      )
+
+  def test_should_pass_lxml_and_xml_patterns_to_find_file_pairs_grouped_by_parent_directory(self):
+    with patch_preprocessing_pipeline() as mocks:
+      opt = get_default_args()
+      opt.base_data_path = 'base'
+      opt.pdf_path = ''
+      opt.lxml_path = 'lxml'
+      opt.xml_path = 'xml'
+      with TestPipeline() as p:
+        mocks['find_file_pairs_grouped_by_parent_directory'].return_value = []
+        configure_pipeline(p, opt)
+
+      mocks['find_file_pairs_grouped_by_parent_directory'].assert_called_with(
+        ['base/lxml', 'base/xml']
+      )
 
 class TestParseArgs(object):
   def test_should_raise_error_without_arguments(self):
