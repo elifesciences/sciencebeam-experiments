a0689c6ecbfd21f440cdb4cd2cedb4ca25ebd09b
Daniel Ecer <de-code@users.noreply.github.com>
2017-11-21 13:06:32 +0000

    moved functions to preprocessing_utils

---
+++ sciencebeam_lab/preprocessing_utils.py
@@ -0,0 +1,239 @@
+from __future__ import absolute_import
+
+import os
+import logging
+from io import BytesIO
+from zipfile import ZipFile, ZIP_DEFLATED
+from itertools import groupby
+from functools import reduce
+
+from lxml import etree
+
+from apache_beam.io.filesystems import FileSystems
+
+from sciencebeam_lab.utils.xml import (
+  xml_from_string_with_recover
+)
+
+from sciencebeam_lab.utils.stopwatch import (
+  StopWatchRecorder
+)
+
+from sciencebeam_lab.beam_utils.io import (
+  dirname,
+  find_matching_filenames,
+  mkdirs_if_not_exists
+)
+
+from sciencebeam_lab.lxml_to_svg import (
+  iter_svg_pages_for_lxml
+)
+
+from sciencebeam_lab.structured_document.svg import (
+  SvgStructuredDocument
+)
+
+from sciencebeam_lab.annotator import (
+  Annotator,
+  DEFAULT_ANNOTATORS
+)
+
+from sciencebeam_lab.alignment.align import (
+  native_enabled as align_native_enabled
+)
+
+from sciencebeam_lab.matching_annotator import (
+  MatchingAnnotator,
+  xml_root_to_target_annotations
+)
+
+from sciencebeam_lab.visualize_svg_annotation import (
+  visualize_svg_annotations
+)
+
+from sciencebeam_lab.blockify_annotations import (
+  annotation_document_page_to_annotation_blocks,
+  merge_blocks,
+  expand_blocks,
+  annotated_blocks_to_image
+)
+
+from sciencebeam_lab.pdf import (
+  PdfToLxmlWrapper,
+  PdfToPng
+)
+
+
+def get_logger():
+  return logging.getLogger(__name__)
+
+def group_files_by_parent_directory(filenames):
+  return {
+    k: list(v)
+    for k, v in groupby(sorted(filenames), lambda x: os.path.dirname(x))
+  }
+
+def zip_by_keys(*dict_list):
+  keys = reduce(lambda agg, v: agg | set(v.keys()), dict_list, set())
+  return (
+    [d.get(k) for d in dict_list]
+    for k in sorted(keys)
+  )
+
+def find_file_pairs_grouped_by_parent_directory(patterns):
+  matching_files_by_pattern = [
+    list(find_matching_filenames(pattern)) for pattern in patterns
+  ]
+  get_logger().info(
+    'found number of files %s',
+    ', '.join(
+      '%s: %d' % (pattern, len(files))
+      for pattern, files in zip(patterns, matching_files_by_pattern)
+    )
+  )
+  patterns_without_files = [
+    pattern
+    for pattern, files in zip(patterns, matching_files_by_pattern)
+    if len(files) == 0
+  ]
+  if patterns_without_files:
+    raise RuntimeError('no files found for: %s' % patterns_without_files)
+  grouped_files_by_pattern = [
+    group_files_by_parent_directory(files) for files in matching_files_by_pattern
+  ]
+  for files_in_group_by_pattern in zip_by_keys(*grouped_files_by_pattern):
+    if all(len(files or []) == 1 for files in files_in_group_by_pattern):
+      yield tuple([files[0] for files in files_in_group_by_pattern])
+    else:
+      get_logger().info(
+        'no exclusively matching files found: %s',
+        [files for files in files_in_group_by_pattern]
+      )
+
+def convert_pdf_bytes_to_lxml(pdf_content, path=None):
+  stop_watch_recorder = StopWatchRecorder()
+
+  stop_watch_recorder.start('convert to lxml')
+  lxml_content = PdfToLxmlWrapper().process_input(
+    pdf_content,
+    '-blocks -noImageInline -noImage -fullFontName'.split()
+  )
+  stop_watch_recorder.stop()
+
+  get_logger().info(
+    'converted to lxml: path=%s, pdf size=%s, lxml size=%s, timings=[%s]',
+    path, format(len(pdf_content), ','), format(len(lxml_content), ','),
+    stop_watch_recorder
+  )
+
+  return lxml_content
+
+def convert_and_annotate_lxml_content(lxml_content, xml_content, xml_mapping, name=None):
+  stop_watch_recorder = StopWatchRecorder()
+
+  stop_watch_recorder.start('parse lxml')
+  lxml_root = etree.fromstring(lxml_content)
+
+  # use a more lenient way to parse xml as xml errors are not uncomment
+  stop_watch_recorder.start('parse xml')
+  xml_root = xml_from_string_with_recover(xml_content)
+
+  stop_watch_recorder.start('extract target annotations')
+  target_annotations = xml_root_to_target_annotations(
+    xml_root,
+    xml_mapping
+  )
+  stop_watch_recorder.stop()
+
+  annotators = DEFAULT_ANNOTATORS + [MatchingAnnotator(
+    target_annotations
+  )]
+  annotator = Annotator(annotators)
+
+  stop_watch_recorder.start('convert to svg')
+  svg_roots = list(iter_svg_pages_for_lxml(lxml_root))
+
+  stop_watch_recorder.start('annotate svg')
+  annotator.annotate(SvgStructuredDocument(svg_roots))
+
+  stop_watch_recorder.start('add visualisation')
+  svg_roots = [
+    visualize_svg_annotations(svg_root)
+    for svg_root in svg_roots
+  ]
+  stop_watch_recorder.stop()
+
+  get_logger().info(
+    'processed: name=%s, lxml size=%s, xml size=%s, timings=[%s] (native align impl=%s)',
+    name, format(len(lxml_content), ','), format(len(xml_content), ','),
+    stop_watch_recorder, align_native_enabled
+  )
+
+  return svg_roots
+
+def relative_path(base_path, path):
+  if not base_path.endswith('/'):
+    base_path += '/'
+  return path[len(base_path):] if path.startswith(base_path) else path
+
+def is_relative_path(path):
+  return not path.startswith('/') and '://' not in path
+
+def join_if_relative_path(base_path, path):
+  return FileSystems.join(base_path, path) if is_relative_path(path) else path
+
+def change_ext(path, old_ext, new_ext):
+  if old_ext is None:
+    old_ext = os.path.splitext(path)[1]
+  if old_ext and path.endswith(old_ext):
+    return path[:-len(old_ext)] + new_ext
+  else:
+    return path + new_ext
+
+def save_pages(output_filename, ext, bytes_by_page):
+  mkdirs_if_not_exists(dirname(output_filename))
+  with FileSystems.create(output_filename) as f:
+    with ZipFile(f, 'w', compression=ZIP_DEFLATED) as zf:
+      for i, data in enumerate(bytes_by_page):
+        page_filename = 'page-%s%s' % (1 + i, ext)
+        get_logger().debug('page_filename: %s', page_filename)
+        zf.writestr(page_filename, data)
+    return output_filename
+
+def save_svg_roots(output_filename, svg_pages):
+  return save_pages(output_filename, '.svg', (
+    etree.tostring(svg_page)
+    for svg_page in svg_pages
+  ))
+
+def pdf_bytes_to_png_pages(pdf_bytes, dpi, image_size):
+  pdf_to_png = PdfToPng(dpi=dpi, image_size=image_size)
+  return (
+    fp.read()
+    for fp in pdf_to_png.iter_pdf_bytes_to_png_fp(pdf_bytes)
+  )
+
+def svg_page_to_blockified_png_bytes(svg_page, color_map, image_size=None):
+  structured_document = SvgStructuredDocument(svg_page)
+  blocks = expand_blocks(
+    merge_blocks(
+      annotation_document_page_to_annotation_blocks(
+        structured_document,
+        structured_document.get_pages()[0]
+      )
+    )
+  )
+  viewbox = svg_page.attrib.get('viewBox')
+  if not viewbox:
+    raise RuntimeError(
+      'viewbox missing on svg, available attributes: %s' % svg_page.attrib.keys()
+    )
+  _, _, width, height = viewbox.split()
+  image = annotated_blocks_to_image(
+    blocks, color_map,
+    width=int(width), height=int(height), background='white',
+    scale_to_size=image_size
+  )
+  out = BytesIO()
+  image.save(out, 'png')
+  return out.getvalue()
